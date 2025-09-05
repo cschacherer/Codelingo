b3e6ca6969f0499bf1d076b8165ad1a659200675
cschacherer <connerschacherer@gmail.com>
2025-09-05 14:39:24 -0600

    getting rid of auto creating application in application.py

--- backend/application.py
+++ backend/application.py
@@ -462,10 +462,9 @@ def createApp(testing=False):
     return application
 
 
-application = createApp()
-
 # main driver function
 if __name__ == "__main__":
     # run() method of Flask class runs the application
     # on the local development server.
+    application = createApp()
     application.run(debug=True)
